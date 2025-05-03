19d2d94c5d67aaad2fe092242b8d6cd90ec39686
Suhani-Singhal2131 <suhanisinghal2131@gmail.com>
2025-05-03 10:06:02 +0530

    adding new files

--- javascript_tut/04.js
+++ javascript_tut/04.js
@@ -1,41 +1,72 @@
-const user={
-    username:"suhani",
-    price: 6788,
-    welcomemessage:function(){
-        console.log(`${this.username} welcome to the website`)
-        console.log(this)//it will print the schema of object user
-        // {
-        //     username: 'suhani',
-        //     price: 6788,
-        //     welcomemessage: [Function: welcomemessage]
-        //   }
+// const user={
+//     username:"suhani",
+//     price: 6788,
+//     welcomemessage:function(){
+//         console.log(`${this.username} welcome to the website`)
+//         console.log(this)//it will print the schema of object user
+//         // {
+//         //     username: 'suhani',
+//         //     price: 6788,
+//         //     welcomemessage: [Function: welcomemessage]
+//         //   }
 
-        //this refer to current context
-    }
-}
- user.welcomemessage()//suhani welcome to the website
+//         //this refer to current context
+//     }
+// }
+//  user.welcomemessage()//suhani welcome to the website
 
-// user.username="hitesh"//changes the current context
-// user.welcomemessage()//hitesh welcome to the website
+// // user.username="hitesh"//changes the current context
+// // user.welcomemessage()//hitesh welcome to the website
 
-console.log(this)//{}
+// console.log(this)//{}
 
-//jab hum node environment ke andar hai koi global context nhi hai isliye empty print kiya
-//lekin browser ke andar global context windows hota hai
+// //jab hum node environment ke andar hai koi global context nhi hai isliye empty print kiya
+// //lekin browser ke andar global context windows hota hai
 
-//arrow function
+// //arrow function
 
-const user1=()=>{
-    let username="suhani"
-    console.log(this.username)//undefined
-    console.log(this)//{}
-}
-user1()
+// const user1=()=>{
+//     let username="suhani"
+//     console.log(this.username)//undefined
+//     console.log(this)//{}
+// }
+// user1()
 
-const add2=(num1,num2)=>{
-    return  num1+num2
-}
-console.log(add2(1,2))
+// const add2=(num1,num2)=>{
+//     return  num1+num2
+// }
+// console.log(add2(1,2))
 
-const add3=(num1,num2)=>num1+num2
-console.log(add3(3,4))
+// const add3=(num1,num2)=>num1+num2
+// console.log(add3(3,4))
+
+//IIFE -Immediately invoked function expression
+(function chai(){
+    //named IIFE
+    console.log("DB CONNECTED")
+})();
+//function that execute immediately
+//global scope ke variable se jo pollution hota hai uslo remove karne ke lie IIFE ka use kiya
+//to write another IIFE function we have to end the previos with semi colon
+((name)=>{
+    //unnamed IIFE
+    console.log(`${name}`)
+})("suhani")
+
+//............
+/*🔵The working of the JavaScript works on three phases:
+1. Global Execution Context
+2. Memory phase / Creation phase
+3. Execution phase
+
+🔵Phase 1: Global Execution Context
+The value of `this` keyword is set to Global Execution Context (which is an empty Object in NODE environment, & window object in browser environment)
+
+🔵Phase 2: Memory phase
+During this phase, JS engine scans the code and assigns the initial values for the variables , functions, etc.. For variables they are 'undefined' and for functions they are set to their function definition
+
+🔵Phase 3: Execution phase
+After the creation phase completed, the execution phase begins where actual values of the variables are set. And when the functions are called it creates a new 'Function Execution' context which consists of two phases (they works same as explained above but only for the particular function scope):
+1. Memory phase 
+2. Execution phase 
+📝The return value of the 'Function Execution' context is passed to the Global Execution Context*/
---
+++ javascript_tut/05.js
@@ -0,0 +1,135 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+}
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
+// for of loop
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+//for in loop
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+//for each loop
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach( function (val){
+    console.log(val);
+} )
+
+coding.forEach( (item) => {
+    console.log(item);
+} )
+
+function printMe(item){
+    console.log(item);
+}
+
+coding.forEach(printMe)
+
+coding.forEach( (item, index, arr)=> {
+    console.log(item, index, arr);
+} )
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
